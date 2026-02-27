813118ae01fb8a78ba5394655f1c696aa89b7dbc
William D. Jerrells iii <Willjerrells@gmail.com>
2026-02-27 11:07:11 -0500

    custom user model.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    # remember that we set the db to equate a empty integer value as null
+    age = models.PositiveIntegerField(null=True, blank=True)
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
+
+
+class UserManagersTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="test_user",
+            email="test@example.com",
+            password="secret",
+            age=39,
+        )
+        self.assertEqual(user.username, "test_user")
+        self.assertEqual(user.email, "test@example.com")
+        self.assertTrue(user.check_password("secret"))
+        self.assertEqual(user.age, 39)
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        s_user = User.objects.create_superuser(
+            username="test_user",
+            email="test@example.com",
+            password="secret",
+            age=39,
+        )
+        self.assertEqual(s_user.username, "test_user")
+        self.assertEqual(s_user.email, "test@example.com")
+        self.assertTrue(s_user.check_password("secret"))
+        self.assertEqual(s_user.age, 39)
+
+        self.assertTrue(s_user.is_staff)
+        self.assertTrue(s_user.is_superuser)
+        self.assertTrue(s_user.is_active)
