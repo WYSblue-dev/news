f067af95c472373fe9ec09ae1e2a0b5f15c57b28
William D. Jerrells iii <Willjerrells@gmail.com>
2026-03-02 19:57:54 -0500

    added the testing means for the pages app home page view.

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class HomePageTests(SimpleTestCase):
+    def test_url_exist_at_exact_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_exist_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("home.html")
+        self.assertContains(response, "Home")
