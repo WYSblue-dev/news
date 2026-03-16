7467c7e406b1b12c02f50c4ad2e82db61bb9ac89
William D. Jerrells iii <Willjerrells@gmail.com>
2026-03-15 22:17:21 -0400

    new styling

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.db.models import Count
 from django.views.generic import TemplateView
+
+from articles.models import Article
 
 # Create your views here.
 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["top_articles"] = (
+            Article.objects.select_related("author")
+            .annotate(comment_total=Count("comment"))
+            .order_by("-comment_total", "-date")[:3]
+        )
+        return context
