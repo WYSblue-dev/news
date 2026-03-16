7411d1f1a98814ce3f8e3c0717c920124abdecca
William D. Jerrells iii <Willjerrells@gmail.com>
2026-03-16 15:50:38 -0400

    added DNS

--- django_project/settings.py
+++ django_project/settings.py
@@ -30,7 +30,16 @@ SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [
+    ".herokuapp.com",
+    "localhost",
+    "127.0.0.1",
+    ".herokuapp.com",
+    "www.forgeandfocus.com",
+    "forgeandfocus.com",
+    "www.forgeandfocus.studio",
+    "forgeandfocus.studio",
+]
 
 
 
